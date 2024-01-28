ac5c9dd35fd902bcc7b0e692d84901e9d49d5835
Carlos Orso <carloseduardoorso1@gmail.com>
2024-01-28 16:13:29 -0300

    primeiras views e templates criados e pagina de login e register com forms

--- reserv/urls.py
+++ reserv/urls.py
@@ -4,5 +4,9 @@ from reserv import views
 app_name = 'reserv'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('eventos/', views.eventos, name='eventos'),
+    path('reserva/', views.reserva, name='reserva'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register_login, name='register')
 ]
--- reserv/views.py
+++ reserv/views.py
@@ -1,10 +1,82 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from reserv.forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+# from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 def index(request):
+    context = {
+        'page': 'home',
+    }
     return render(
         request,
-        'reserv/index.html'
+        'reserv/index.html',
+        context
+    )
+
+
+def eventos(request):
+    context = {
+        'page': 'eventos',
+    }
+    return render(
+        request,
+        'reserv/eventos.html',
+        context
+    )
+
+
+def reserva(request):
+    context = {
+        'page': 'reservas',
+    }
+    return render(
+        request,
+        'reserv/reserva.html',
+        context
+    )
+
+
+def login(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            return redirect('reserv:index')
+
+    context = {
+        'page': 'login',
+        'form': form
+    }
+    return render(
+        request,
+        'reserv/login.html',
+        context
+    )
+
+
+def register_login(request):
+    form = RegisterForm()
+    context = {
+        'form': form,
+        'page': 'register',
+    }
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('reserv:login')
+
+    return render(
+        request,
+        'reserv/register.html',
+        context
     )
